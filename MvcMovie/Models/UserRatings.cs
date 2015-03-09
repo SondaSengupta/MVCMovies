e8dd79d1ed78a2162593fa6b804dfb75e0e5c16b
evanroane <evan.roane@gmail.com>
2015-03-09 13:25:25 -0500

    added model item

---
+++ MvcMovie/Models/UserRatings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace MvcMovie.Models
+{
+    public class UserRating
+    {
+        public int ID { get; set; }
+
+        public string UserId { get; set; }
+        
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Display(Name = "User Rating")]
+        [Range(1, 5)]
+        public decimal UsrRating { get; set; }
+    }
+
+    public class UserRatingsDBContext : DbContext
+    {
+        public DbSet<UserRating> Ratings { get; set; }
+    }
+
+}
